43cd1937658be6a2f8548df22c6ec8f1363ab21c
stacysn <stacysuen5@gmail.com>
2017-08-04 22:19:38 -0700

    step 2 adding ui for home

---
+++ src/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Search extends Component {
+  render(){
+    return (
+      <div className="Search">
+          <form>
+            <input/>
+            <button type='submit'> Submit </button>
+          </form>
+      </div>
+    )
+  }
+}
+
+export default Search;
